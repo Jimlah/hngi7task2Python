aee0ca5d3033f3b4c3b751c268448a3705646727
sirrobot01 <akeremukhtar10@gmail.com>
2020-06-08 21:29:11 +0100

    Adds post ree

--- app/api.py
+++ app/api.py
@@ -1,9 +1,16 @@
 from flask import Flask
-from flask_restx import Resource, Api
+from flask_restx import Resource, Api, reqparse
+from werkzeug.datastructures import FileStorage
+from flask_cors import CORS
 
 app = Flask(__name__)  # Create a Flask WSGI application
+CORS(app)
 api = Api(app, doc='/docs')  # Create a Flask-RESTPlus API
 ns_conf = api.namespace('', description='Image Resizer operations')
+
+upload_parser = reqparse.RequestParser()
+upload_parser.add_argument('image', location='files',
+                           type=FileStorage, required=True)
 
 
 @ns_conf.route('/image/')  # Create a URL route to this resource
@@ -15,11 +22,20 @@ class ImageResizer(Resource):  # Create a RESTful resource
         """
         return {'hello': 'world'}
 
+    @ns_conf.expect(upload_parser)
     def post(self):
         """
 
         :return: Image created
         """
+
+        args = upload_parser.parse_args()
+        if "image" in args['image'].mimetype:
+            img = args["image"]
+            # do_something_here()
+
+            return {"success": True}
+        return {"success": False, "message": "Incorrect file type specified"}
 
 
 if __name__ == '__main__':
