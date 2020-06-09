8f08eabcfd96f37cd3a9fb616ebf128c1de72b35
sirrobot01 <akeremukhtar10@gmail.com>
2020-06-09 20:31:05 +0100

    updates api.py'

--- app/api.py
+++ app/api.py
@@ -1,8 +1,9 @@
-from flask import Flask
+import resizer
+from flask import Flask, request
+from flask_cors import CORS
 from flask_restx import Resource, Api, reqparse
 from werkzeug.datastructures import FileStorage
-from flask_cors import CORS
-
+import os
 app = Flask(__name__)  # Create a Flask WSGI application
 CORS(app)
 api = Api(app, doc='/docs')  # Create a Flask-RESTPlus API
@@ -11,6 +12,8 @@ ns_conf = api.namespace('', description='Image Resizer operations')
 upload_parser = reqparse.RequestParser()
 upload_parser.add_argument('image', location='files',
                            type=FileStorage, required=True)
+upload_parser.add_argument('width', type=int, required=False)
+upload_parser.add_argument('height', type=int, required=False)
 
 
 @ns_conf.route('/image/')  # Create a URL route to this resource
@@ -30,11 +33,19 @@ class ImageResizer(Resource):  # Create a RESTful resource
         """
 
         args = upload_parser.parse_args()
+        height, width = None, None
+        if args["height"]:
+            height = args["height"]
+        if args["width"]:
+            width = args["width"]
         if "image" in args['image'].mimetype:
-            img = args["image"]
-            # do_something_here()
-
-            return {"success": True}
+            file = request.files["image"]
+            filepath = "{}".format(file.filename)
+            file.save(filepath)
+            result = resizer.image_upload(file=filepath, height=height, width=width)
+            result.update({"success": True})
+            os.remove(filepath)
+            return result
         return {"success": False, "message": "Incorrect file type specified"}
 
 
--- app/resizer.py
+++ app/resizer.py
@@ -1,26 +1,33 @@
 import cloudinary
 from cloudinary import uploader
-from cloudinary import CloudinaryImage
 
 cloudinary.config(
-  cloud_name = 'hngi7-python',  
-  api_key = '847136641947533',  
-  api_secret = '3VQy3iuna0qAct-ShP7yfAwJfzc'  
+    cloud_name='hngi7-python',
+    api_key='847136641947533',
+    api_secret='3VQy3iuna0qAct-ShP7yfAwJfzc'
 )
 
 
 def image_upload(file, **options):
-    cloudinary.uploader.upload(file, public_id= file[:-4])
-    resize(file, width=100, height= 150)
+    res = uploader.upload(file, public_id=file.split(".")[0])
+    if options['width']:
+        width = options['width']
+    else:
+        width = 150
+    if options['height']:
+        height = options['height']
+    else:
+        height = 150
+    return resize(file, width=width, height=height, res=res)
 
-    
+
 # this function retrives the uploaded image and and edits it returns a url to the uploaded image
-def resize(file, width, height):
-    result = cloudinary.utils.cloudinary_url(file,
-                                width = width,
-                                height = height,
-                                )
-    print(result)
-    
-#test
-#image_upload("test_3.jpg")
+def resize(file, width, height, res):
+    result = cloudinary.utils.cloudinary_url(file.split(".")[0],
+                                             width=width,
+                                             height=height,
+                                             )
+    resp = {"url": result[0], 'filename': res['original_filename'], "height": height, "width": width,
+            'created_at': res['created_at']}
+
+    return resp
